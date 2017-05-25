87eb19242a78c719879fa38f7fb60ec231302b16
BK <bkam03@yahoo.com>
2017-05-24 20:17:52 -1000

    basic module structure created, prototype addValue to cement variables in memory

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,44 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  //private var
+  var list = {};
+  var _listHead = null;
+  var _listTail = null;
 
+  //private methods
+  function _setListHead(newHeadNode) {
+    _listHead = newHeadNode;
+  }
+  function _setListTail(newTailNode) {
+    _listTail = newTailNode;
+  }
+
+  //publ var
+
+
+  //publ methods
+  function getHead() {
+
+  }
+
+  function addValue(value) {
+    var newNode = {
+      value : value,
+      nextNode : null
+    };
+    if( _listHead === null ) {// if list is empty, make this the head
+      _listHead = newNode;
+    } else {              //otherwise append to tail node
+      _listTail.nextNode = newNode;
+    }
+    _listTail = newNode;
+
+
+  }
+
+  return {
+    getHead,
+    addValue
+  };
 }
